f9e160a17f068bf4e6a61783f7a00c162a301326
Pasindu Piyumika <135522401+nvppiyumika@users.noreply.github.com>
2023-07-14 12:02:20 +0530

    Add files via upload

---
+++ php file handling/a.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <?php
+        $a=$_REQUEST["un"];
+        echo "Username is: $a";
+        $b=$_REQUEST["pwd"];
+        echo "<br>Password is: $b";
+        ?>
+    </body>
+</html>
